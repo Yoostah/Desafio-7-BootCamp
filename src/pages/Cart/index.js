f0b704aa3806b47edbb67d9d7ae5181a7edb51d6
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-10-01 00:22:40 -0300

    estilizacao do carrinho finalizada - sem consulta

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,27 +1,80 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
-  Carrinho,
-  CarrinhoText,
-  BotaoVoltar,
-  BotaoVoltarText,
+  Products,
+  Product,
+  BotaoFinalizar,
+  BotaoFinalizarText,
+  ProductInfo,
+  ProductImage,
+  Price,
+  ProductDescription,
+  ProductActions,
+  ItemQuantity,
+  Actions,
+  ItemTotal,
+  TotalProducts,
+  TotalProductsText,
+  TotalProductsValue,
 } from './styles';
 
 export default class Cart extends Component {
-  state = {};
+  state = {
+    products: [
+      {
+        id: 1,
+        title: 'Tênis de Caminhada Leve Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+      },
+      {
+        id: 2,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+      },
+    ],
+  };
 
   render() {
     const { navigation } = this.props;
+    const { products } = this.state;
+
     return (
       <Container>
-        <Carrinho>
-          <CarrinhoText>Carrinho</CarrinhoText>
-        </Carrinho>
-        <BotaoVoltar onPress={() => navigation.navigate('Main')}>
-          <BotaoVoltarText>Voltar</BotaoVoltarText>
-        </BotaoVoltar>
+        <Products>
+          {products.map(product => (
+            <>
+              <Product key={product.id}>
+                <ProductImage source={{ uri: product.image }} />
+                <ProductInfo>
+                  <ProductDescription>{product.title}</ProductDescription>
+                  <Price>R${product.price}</Price>
+                </ProductInfo>
+                <Icon name="delete-forever" size={24} color="#7159C1" />
+              </Product>
+              <ProductActions>
+                <Actions>
+                  <Icon name="delete-forever" size={24} color="#7159C1" />
+                  <ItemQuantity>0</ItemQuantity>
+                  <Icon name="delete-forever" size={24} color="#7159C1" />
+                </Actions>
+                <ItemTotal>R$567.99</ItemTotal>
+              </ProductActions>
+            </>
+          ))}
+          <TotalProducts>
+            <TotalProductsText>Total</TotalProductsText>
+            <TotalProductsValue>R$1619,10</TotalProductsValue>
+          </TotalProducts>
+          <BotaoFinalizar onPress={() => navigation.navigate('Main')}>
+            <BotaoFinalizarText>Finalizar Pedido</BotaoFinalizarText>
+          </BotaoFinalizar>
+        </Products>
       </Container>
     );
   }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -3,31 +3,95 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   background: #111;
+  padding: 20px;
 `;
 
-export const Carrinho = styled.View`
+export const Products = styled.View`
   background: #fff;
-  padding: 10px;
-  margin: 15px;
   border-radius: 4px;
 `;
-
-export const CarrinhoText = styled.Text`
-  text-align: center;
+export const Product = styled.View`
+  padding: 15px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
-export const BotaoVoltarText = styled.Text`
+
+export const BotaoFinalizarText = styled.Text`
   color: #fff;
   text-align: center;
   font-weight: bold;
   text-transform: uppercase;
 `;
 
-export const BotaoVoltar = styled(RectButton)`
+export const BotaoFinalizar = styled(RectButton)`
   background: #7159c1;
   border: 0;
   border-radius: 4px;
   overflow: hidden;
-  margin: auto 15px;
   padding: 10px;
+  margin: 10px;
   align-items: center;
+`;
+
+export const ProductInfo = styled.View`
+  flex: 1;
+  margin-left: 10px;
+`;
+export const ProductDescription = styled.Text`
+  font-size: 14px;
+  color: #666;
+`;
+export const Price = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 5px;
+`;
+export const ProductImage = styled.Image`
+  width: 70px;
+  height: 70px;
+  background: #eee;
+`;
+export const ProductActions = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  background: #ddd;
+  margin: 0 10px 20px;
+  align-items: center;
+  padding: 6px;
+  border-radius: 4px;
+`;
+export const ItemQuantity = styled.Text`
+  border: 1px solid #666;
+  border-radius: 4px;
+  color: #333;
+  padding: 5px;
+  width: 50px;
+  background: #fff;
+`;
+
+export const Actions = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const ItemTotal = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const TotalProducts = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px;
+`;
+export const TotalProductsText = styled.Text`
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #ccc;
+  font-weight: bold;
+`;
+export const TotalProductsValue = styled.Text`
+  font-weight: bold;
+  font-size: 22px;
 `;
